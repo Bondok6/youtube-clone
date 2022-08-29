5b8300b931c161434fcea21d68acebfaf4c4307b
Kyrillos Hany <kyrillos.hany@hamza.solutions>
2022-08-29 19:41:59 +0200

    Finish Sidebar and Navbar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,49 @@
+import { Stack, Box, Typography } from '@mui/material';
+import { Sidebar, Videos } from './';
+
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          CopyRight 2022 - Kyrillos Hany
+        </Typography>
+      </Box>
+      <Box
+        p={2}
+        sx={{
+          overflowY: 'auto',
+          height: '90vh',
+          flex: 2,
+        }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: '#fff',
+          }}
+        >
+          New <span style={{ color: '#F31503' }}>Videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
