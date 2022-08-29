0642615363fde7f8cf767325e3918cfd59927de5
Kyrillos Hany <kyrillos.hany@hamza.solutions>
2022-08-29 18:58:59 +0200

    Add Basic components and utils files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
-  );
-}
+import {
+  Navbar,
+  Feed,
+  VideoDetails,
+  ChannelDetails,
+  SearchFeed,
+} from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ background: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetails />} />
+        <Route path="/channel/:id" element={<ChannelDetails />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
